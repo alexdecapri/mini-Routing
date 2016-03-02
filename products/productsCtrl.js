613236e0e98d40eadcbbb999b892e07628053e26
alexdecapri <alexdecapri@gmail.com>
2016-03-01 19:49:55 -0700

    finished

--- products/productsCtrl.js
+++ products/productsCtrl.js
@@ -1,5 +1,12 @@
 var app = angular.module("miniRouting");
 
-app.controller("productsCtrl", function($scope) {
+app.controller("productsCtrl", function($scope, $stateParams, productsService) {
+
+  if ($stateParams.id === "shoes") {
+    $scope.productData = productsService.shoeData;
+  }
+  else if ($stateParams.id === "socks") {
+    $scope.productData = productsService.sockData;
+  }
 
 });
